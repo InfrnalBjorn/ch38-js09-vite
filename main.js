ae3070e616a01450721134dd5cbfc2d53334bc31
Pedro Mendoza <pedro.mendoza.b14@gmail.com>
2024-03-15 15:39:22 -0600

    add: h2

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ import "./styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+import { getUsersUsignFetch } from "./src/js/getUsers";
+
+const getUsers = async () => {
+  const users = await getUsersUsignFetch("https://reqres.in/api/users?page=2");
+  console.log(users);
+};
+
+getUsers();
